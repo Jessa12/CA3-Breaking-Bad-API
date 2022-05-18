6ae4b3cabeffb53ef2702ff2c0695ecdc1cc8ed4
Jessa12 <jhinong12@gmail.com>
2022-05-18 23:10:17 +0000

    Still at quote

--- src/app/pages/deaths/deaths.page.ts
+++ src/app/pages/deaths/deaths.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-deaths',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeathsPage implements OnInit {
 
-  constructor() { }
+  death: Observable<any>;
+  deathId: Observable<any>;
+
+  constructor(private router: Router, private api: ApiService) { }
 
   ngOnInit() {
+      this.death = this.api.getDeaths();
+      this.death.subscribe(data => {
+      console.log('my deaths: ', data);
+    });
   }
 
+  openDetails(character){
+      let deathId = this.deathId;
+      this.router.navigateByUrl(`/tabs/deaths/${deathId}`);
+  }
 }
